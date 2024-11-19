9467dfe041fad223ee094bff7bdf49b391cc8c83
Rouven <30431753+RouBroeck@users.noreply.github.com>
2024-11-19 17:56:30 +0100

    Add Google Chart and Bootstrap

--- src/vuejs/src/App.vue
+++ src/vuejs/src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand fixed-top navbar-dark bg-dark p-2">
+    <a class="navbar-brand" href="">
+      Bathroom-Scale
+    </a>
+    <ul class="navbar-nav me-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#current">Current</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#week">Week</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#month">Month</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="d-inline-flex flex-column">
+    <div class="card" style="width: 48rem;">
+      <h1 id="current"> Current </h1>
+      <Graph data="[10,10,10]"/>
+    </div>
+    <div class="card" style="width: 48rem;">
+      <h1 id="week"> Week </h1>
+      <Graph data="[10,10,10]"/>
+    </div>
+    <div class="card" style="width: 48rem;">
+      <h1 id="month"> Month </h1>
+      <Graph data="[10,10,10]"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Graph from './components/Graph.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Graph,
   }
 }
+
 </script>
 
 <style>
---
+++ src/vuejs/src/components/Graph.vue
@@ -0,0 +1,42 @@
+<template>
+  <GChart
+    type="LineChart"
+    :data="chartData"
+    :options="chartOptions"
+  />
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    data: String
+  },
+  components: {
+    GChart
+  },
+  data () {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+        ['Seconds', 'Weight'],
+        ['-60', 66],
+        ['-50', 62],
+        ['-40', 70],
+        ['-30', 56]
+      ],
+      chartOptions: {
+        chart: {
+          title: 'Company Performance',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
